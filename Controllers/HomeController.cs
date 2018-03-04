58b6edef0f3c90f627268376925852ad18b82f2c
Edgar Fabian Gonzalez Peña <edgargonzalez.developer@gmail.com>
2018-03-04 01:11:18 +0000

    Initial Commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace edgargonzalez.Controllers
+{
+    public class HomeController :Controller
+    {
+
+        public IActionResult Index()
+        {
+            ViewBag.Title = "Edgar Gonzalez";
+            return View();
+        }
+    }
+}
